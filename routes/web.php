a67e3cb93bc11354b6167e49c6289d5ef948923c
myatmin001 <myatmin.dev001@gmail.com>
2023-11-22 09:51:51 +0900

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Book;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/books', function () {
+    //return view('welcome');
+});
+
+Route::delete('/book/{book}', function (Book $book) {
+    //return view('welcome');
+});
